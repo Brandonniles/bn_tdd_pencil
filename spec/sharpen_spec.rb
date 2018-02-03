2b57d051ca0558864b6a3448e187196c2e68a487
Brandon O. Niles <b21niles@gmail.com>
2018-02-03 10:39:59 -0500

    wrote test to make sure max dura and dura were equal upon the creation of a new pencil

--- spec/sharpen_spec.rb
+++ spec/sharpen_spec.rb
@@ -2,6 +2,7 @@ require_relative '../lib/pencil'
 
  describe Pencil do
    let(:sp) { Pencil.new(paper: '', dura: 20)}
+   let(:equal_p) { Pencil.new(paper: '', dura: 88) }
 
    it "should have a sharpen method" do
      expect(sp.respond_to?(:sharpen)).to eq(true)
@@ -10,6 +11,12 @@ require_relative '../lib/pencil'
    context 'instance variable for durability initial value should exist' do
      it "@max_dura should exist" do
        expect(sp.respond_to?(:max_dura)).to eq(true)
+     end
+   end
+
+   context 'a new pencil should have equal dura and max_dura values' do
+     it "max_dura and dura should be equal" do
+       expect(equal_p.dura).to eq(equal_p.max_dura)
      end
    end
 
