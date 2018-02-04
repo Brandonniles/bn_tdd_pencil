9f61682d0ae22f7a928bbd9d78e36c89907a5fec
Brandon O. Niles <b21niles@gmail.com>
2018-02-03 21:34:44 -0500

    wrote and passed tests for partial deletion is eraser durability is greater than the length of the string to be deleted. This stage also had a fair amount of refactoring

--- lib/pencil.rb
+++ lib/pencil.rb
@@ -29,15 +29,30 @@ class Pencil
     @length > 0 ? (@dura = @max_dura) && (@length -= 1) : "Cannot sharpen, length is 0"
   end
 
+  def complete_deletion(rev_str, rev_paper)
+    rev_str.length.times { rev_paper.insert(rev_paper.index(rev_str), ' ') }
+    rev_paper.slice!(rev_str)
+    @paper = rev_paper.reverse
+
+  end
+
+  def partial_deletion(rev_str, rev_paper)
+    # add a space(s)
+    @eraser_dura.times { rev_paper.insert(rev_paper.index(rev_str), ' ') }
+    # delete a character(s)
+    rev_paper.slice!(rev_paper.index(rev_str), @eraser_dura)
+    # update eraser_dura
+    @paper = rev_paper.reverse
+
+  end
+
   def erase(take_input)
     rev_str = take_input.reverse
     rev_paper = @paper.reverse
-    if @eraser_dura > 0
-      rev_str.length.times { rev_paper.insert(rev_paper.index(rev_str), ' ') }
-      rev_paper.slice!(rev_str)
-      @paper = rev_paper.reverse
-      take_input.each_char { |chr| @eraser_dura -= 1 if chr =~ /\S/ }
-    end
+    complete_deletion(rev_str, rev_paper) if @eraser_dura >= rev_str.length
+    partial_deletion(rev_str, rev_paper) if (0...rev_str.length).include?(@eraser_dura)
+    take_input.each_char { |chr| @eraser_dura -= 1 if chr =~ /\S/ }
+    puts 'Eraser is empty' if @eraser_dura <= 0
   end
 
 end
